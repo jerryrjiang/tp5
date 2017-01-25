7cb14b1307af672263bfa3968353654f4fb93ee7
jerry <373662434@qq.com>
2017-01-26 00:47:51 +0800

    20170126

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,12 +2,14 @@
 namespace app\index\controller;
 
 use think\Controller;
+use think\Db;
 
 class Index extends Controller
 {
     public function hello($name = 'thinkphp')
     {
-        $this->assign('name', $name);
+        $data = Db::name('data')->find();
+        $this->assign('result', $data);
         return $this->fetch();
     }
 }
