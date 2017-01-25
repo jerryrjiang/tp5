eba4f9dcf908465690be5f3a107c2417031fbcf1
jerry <373662434@qq.com>
2017-01-25 21:59:15 +0800

    20170125

---
+++ git_synchronous.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#-------------CopyRight-------------
+#Name: code synchronous
+#Version: 1.0.0
+#Type: Subversion Maintance 
+#Language: bash shell  
+#Date: 2017-1-25  
+#Author: Jerryrjiang 
+
+tp_path="/var/www/html/tp5"
+ci_time=`date +%Y%m%d`
+github_path="https://github.com/jerryrjiang/tp5.git"
+cd $tp_path
+git add ./*
+git commit -m "$ci_time"
+git remote add origin $github_path
+git push -u origin master
